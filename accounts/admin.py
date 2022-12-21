92ef87f6135c050797f81ca7c4ed3e2af6f943f8
0xMarvell <rokemarvellous@gmail.com>
2022-12-21 18:57:24 +0100

    - created new user model - changed auth from username login to email login

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
 
 # Register your models here.
+
+class UserAdminConfig(UserAdmin):
+    ordering = ('-start_date',)
+    list_display = ('first_name', 'last_name', 'email','is_staff', 'is_patient', 'is_health_worker')
+    fieldsets = (
+        (None, {'fields': ('email', 'first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_patient', 'is_health_worker')}),
+        # ('Personal', {'fields': ('about',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'first_name', 'last_name',
+                'phone_number','is_patient', 'is_health_worker',
+                'password1', 'password2', 'is_active', 'is_staff',
+                )}
+         ),
+    )
+
+admin.site.register(User, UserAdminConfig)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,53 @@
+from django.utils import timezone
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 # Create your models here.
+
+
+class CustomAccountManager(BaseUserManager):
+    def create_superuser(self, email, first_name, last_name, phone_number, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(email, first_name, last_name, phone_number, password, **other_fields)
+
+    def create_user(self, email, first_name, last_name, phone_number, password, **other_fields):
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,first_name=first_name, 
+            last_name=last_name, phone_number=phone_number, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True, blank=False)
+    first_name = models.CharField(max_length=150, blank=False)
+    last_name = models.CharField(max_length=150, blank=False)
+    phone_number = models.IntegerField(unique=True, blank=False)
+    start_date = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_patient = models.BooleanField('patient', default=False)
+    is_health_worker = models.BooleanField('health worker', default=False)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number']
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
